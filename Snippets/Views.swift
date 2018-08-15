1c540b5d64743f157eedc2342b8eeaa9382d2879
Warren Hansen <whansen1@me.com>
2018-08-15 12:34:06 -0500

    task: added views

---
+++ Snippets/Views.swift
@@ -0,0 +1,65 @@
+//
+//  UIView.swift
+//  Lifehacks
+//
+//  Created by Matteo Manferdini on 26/01/2018.
+//  Copyright © 2018 Matteo Manferdini. All rights reserved.
+//
+
+import UIKit
+
+extension CGRect {
+    func shifted(by x: CGFloat) -> CGRect {
+        var newRect = self
+        newRect.origin.x += x
+        return newRect
+    }
+}
+
+@IBDesignable class RoundImageView: UIImageView {}
+
+@IBDesignable
+class RoundTextField: UITextField {
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.shifted(by: 12.0)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.shifted(by: 12.0)
+    }
+}
+
+@IBDesignable class RoundTextView: UITextView {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        textContainerInset = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+    }
+}
+
+extension UIView {
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get { return layer.cornerRadius }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get { return layer.borderWidth }
+        set { layer.borderWidth = newValue }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            guard let color = layer.borderColor else {
+                return nil
+            }
+            return UIColor(cgColor: color)
+        }
+        set { layer.borderColor = newValue?.cgColor }
+    }
+}
