d120e78a46d55ced6c1081fefb75d0cb82d964f0
Warren Hansen <whansen1@me.com>
2018-08-22 09:02:46 -0500

    task: added circle view

--- Snippets/Custom View Shapes.swift
+++ Snippets/Custom View Shapes.swift
@@ -1,82 +1,21 @@
 //
-//  UIView.swift
-//  Lifehacks
+//  Custom View Shapes.swift
+//  Snippets
 //
-//  Created by Matteo Manferdini on 26/01/2018.
-//  Copyright © 2018 Matteo Manferdini. All rights reserved.
+//  Created by Warren Hansen on 8/20/18.
+//  Copyright © 2018 Warren Hansen. All rights reserved.
 //
 
 import UIKit
 
-extension CGRect {
-    func shifted(by x: CGFloat) -> CGRect {
-        var newRect = self
-        newRect.origin.x += x
-        return newRect
-    }
-}
-
-@IBDesignable class RoundImageView: UIImageView {}
-
-@IBDesignable class RoundTextField: UITextField {
-    override func textRect(forBounds bounds: CGRect) -> CGRect {
-        return bounds.shifted(by: 12.0)
-    }
-    
-    override func editingRect(forBounds bounds: CGRect) -> CGRect {
-        return bounds.shifted(by: 12.0)
-    }
-}
-
-@IBDesignable class RoundTextView: UITextView {
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        textContainerInset = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
-    }
-}
-
-extension UIView {
-    @IBInspectable
-    var cornerRadius: CGFloat {
-        get { return layer.cornerRadius }
-        set {
-            layer.cornerRadius = newValue
-            layer.masksToBounds = newValue > 0
-        }
-    }
-    
-    @IBInspectable
-    var borderWidth: CGFloat {
-        get { return layer.borderWidth }
-        set { layer.borderWidth = newValue }
-    }
-    
-    @IBInspectable
-    var borderColor: UIColor? {
-        get {
-            guard let color = layer.borderColor else {
-                return nil
-            }
-            return UIColor(cgColor: color)
-        }
-        set { layer.borderColor = newValue?.cgColor }
-    }
-}
-
 @IBDesignable class Triangle: UIView {
     
     @IBInspectable var color: UIColor = .red
-    
     @IBInspectable var firstPointX: CGFloat = 0
-    
     @IBInspectable var firstPointY: CGFloat = 0
-    
     @IBInspectable var secondPointX: CGFloat = 0.5
-    
     @IBInspectable var secondPointY: CGFloat = 1
-    
     @IBInspectable var thirdPointX: CGFloat = 1
-    
     @IBInspectable var thirdPointY: CGFloat = 0
     
     override func draw(_ rect: CGRect) {
@@ -128,5 +67,5 @@ extension UIView {
         layer.cornerRadius = bounds.size.width / 2
         layer.masksToBounds = true
     }
+    
 }
-
